30983f5356dd1b3a96ab6477876975dd571d60e3
Sybren Keizer <92291687+sybrenkeizer@users.noreply.github.com>
2022-10-25 22:06:45 +0545

    Fundament finsihed

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ const voiceMenuEl = document.getElementById('custom-input__voice-menu');
 const textAreaEl = document.getElementById('custom-input__text-input');
 const submitBtn = document.getElementById('custom-input__submit-btn');
 const imageGridEl = document.getElementById('image-grid');
+const settingsBtn = document.getElementById('main-header__settings-btn');
+const overlay = document.getElementById('overlay');
+
+
+const DEFAULT_LANGUAGE = 'Google UK English Male';
 
 const data = [
 	{
@@ -55,19 +60,118 @@ const data = [
 ];
 
 
+
+// ===== Populate grid ===== //
 const createCell = (item) => {
   const div = document.createElement('div');
   const { image, text } = item;
 
   div.className = 'image-grid__cell';
+  div.dataset.text = text;
   div.innerHTML = `
-    <img src="${image}" alt="${text}" />
+    <img src="${image}" alt="${text}"/>
     <p>${text}</p>
+    <div class="img-overlay">
+    </div>
+    <i class="fa-solid fa-volume-high audio-icon"></i>
+
   `
-
   imageGridEl.appendChild(div);
-} 
-
-
+};
 
 data.forEach(createCell);
+
+
+
+// ===== Input check & notify ===== //
+const showNotification = (notification) => {
+	const popup = document.getElementById('popup');
+	if (notification === 'input-error') {
+		popup.textContent = 'Please enter the text to be read';
+	} else {
+		return;
+	}
+	popup.classList.add('show');
+	overlay.classList.add('show');
+	setTimeout(() => {
+		popup.classList.remove('show');
+		overlay.classList.remove('show');
+	}, 3500);
+};
+
+const validateForm = () => {
+	if (textAreaEl.value === '') {
+		showNotification('input-error');
+		setTextMessage('You did not enter any text');
+  	speakText();
+	};
+};
+
+
+
+// ===== Open settings ===== //
+const showSettings = () => {
+	const settingsEl = document.getElementById('settings');
+	const settingsCloseBtn = document.getElementById('settings-header__close-btn');
+	settingsEl.classList.add('show');
+	overlay.classList.add('show');
+	settingsCloseBtn.onclick = () => {
+		settingsEl.classList.remove('show');
+		overlay.classList.remove('show');
+	};
+
+}
+
+
+
+// ===== Speak text ===== // 
+const message = new SpeechSynthesisUtterance;
+const setTextMessage = (text) => message.text = text; 
+const speakText = () => speechSynthesis.speak(message)
+
+const readImageBox = (e) => {
+  setTextMessage(e.target.parentElement.dataset.text);
+  speakText();
+};
+
+const readInputBox = (e) => {
+	e.preventDefault();
+	validateForm();
+  setTextMessage(textAreaEl.value);
+  speakText();
+};
+
+
+
+// ===== Voices ===== //
+let voices = [];
+
+const getVoices = () => {
+  voices = speechSynthesis.getVoices();
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+		voice.name === DEFAULT_LANGUAGE && (option.selected = true);
+		// Capture group anything (.*) after "Google"
+		const editName = () => /Google(.*)/.exec(voice.name)[1];
+		option.textContent = `${editName()} ${voice.lang}`;
+    voiceMenuEl.appendChild(option);
+		message.voice = voices.find(voice => voice.name === DEFAULT_LANGUAGE);
+  });
+};
+
+const setVoice = (e) => message.voice = voices.find(voice => voice.name === e.target.value);
+
+
+
+// ===== Event listeners ===== //
+settingsBtn.addEventListener('click', showSettings);
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+voiceMenuEl.addEventListener('change', setVoice);
+submitBtn.addEventListener('click', readInputBox);
+imageGridEl.addEventListener('click', readImageBox);
+window.addEventListener('DOMContentLoaded', () => {
+	getVoices();
+	setVoice();
+});
+
